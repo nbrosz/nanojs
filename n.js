467f22b4933416fa061c2f16fba79b1ce79d2868
Nic Brosz <nbrosz@nicbrosz.com>
2016-08-24 21:40:18 -0500

    Sets up inheritance among engine sub-classes and adds origin-offsetting to Text object.

--- n.js
+++ n.js
@@ -53,7 +53,7 @@ var Njs=function(id,cw,ch,aisrc,ta,fr,gs,aa){ //_ implies important hidden membe
 			if(N.R)lf(_Gl);//loop as long as game is still running
 		},
 		//draw texture: x, y, atlas index, texture index, options
-		//options: sc - scale, al - alpha, ng - rotation angle, ox - offset x, oy - offset y, fx - flip x, fy - flip y
+		//options: sc - scale, al - alpha, ng - rotation angle, ox - offset x, oy - offset y, fx - flip x, fy - flip y, ow - overall width, oh - overall height
 		_Dtex=function(x,y,ai,ti,o){
 			cx.save();//preserve default scale/translation values
 			// set up optional parameter defaults
@@ -77,14 +77,16 @@ var Njs=function(id,cw,ch,aisrc,ta,fr,gs,aa){ //_ implies important hidden membe
 			yo=yi*(a[4]+sp),//y-offset for the texture's current frame (including padding)
 			tw=a[3]*o.sc,//texture width
 			th=a[4]*o.sc,//texture height
+			ow=o.ow||tw,//overall width
+			oh=o.oh||th,//overall height
 
 			//rotation
 			d2r=Math.PI/180,//degrees to radians
 			ra=o.ng*d2r,//texture rotation angle to radians
 			aco=Math.cos(-ra),//cos of angle to compensate for canvas x rotation
 			asi=Math.sin(-ra),//sin of angle to compensate for canvas y rotation
-			tx=x*aco-y*asi-tw*o.ox,//formula to compensate for canvas x rotation
-			ty=y*aco+x*asi-th*o.oy;//formula to compensate for canvas y rotation
+			tx=x*aco-y*asi-ow*o.ox,//formula to compensate for canvas x rotation
+			ty=y*aco+x*asi-oh*o.oy;//formula to compensate for canvas y rotation
 			cx.rotate(ra);
 
 			//flipping
@@ -121,6 +123,32 @@ var Njs=function(id,cw,ch,aisrc,ta,fr,gs,aa){ //_ implies important hidden membe
 				if(o[i])o[i]=1; //set key state to held
 				else delete o[i]; //set key state to not pressed
 			}
+		},
+		//Private Classes
+		//Gameobject (common base class): x, y, options
+		//options: f=current frame, ox=origin offset x, oy=origin offset y, fx=x-flip, fy=y-flip
+		_Go=function(x,y,o){
+			var I=this;
+			I.Init=function(x,y,o) {
+				o=o||{};
+				I.x=x;//x position
+				I.y=y;//y position
+				I.ox=o.ox||0;//offset x
+				I.oy=o.oy||0;//offset y
+				I.fx=!!o.fx;//flip x?
+				I.fy=!!o.fy;//flip y?
+				I.ng=o.ng||0;//rotation angle
+				I.sc=o.sc||1;//scale
+				I.al=isNaN(o.al)?1:o.al;//sprite alpha
+				return o;//return options object for reuse
+			};
+			I.Sz=function(ti){//get gameobject size
+				if(!N._ai)return [0,0];//safety
+				var ta=N.Ta[ti];//texture atlas for the current gameobject
+				//sc=N.GS*I.sc;
+				return [ta[3]*I.sc,ta[4]*I.sc];//return width,height of current sprite, scaled
+			};
+			_Gol.push(I);
 		};
 
 		_ai.src=aisrc;//set atlas image src
@@ -162,19 +190,68 @@ var Njs=function(id,cw,ch,aisrc,ta,fr,gs,aa){ //_ implies important hidden membe
 		N.Ak=function(s){for(var ki in N.ko){if(N.ko[ki]==s)return ki;}return 0;}// return any key with the desired state
 
 		//Public Classes
+		//sprite: x, y, spritesheet, current animation (texture atlas index), options
+		//options: f=current frame, ox=origin offset x, oy=origin offset y, fx=x-flip, fy=y-flip
+		N.Spr=function(x,y,s,ti,o){
+			var I=this,oi,os,_ft;
+			_Go.call(I,x,y,o);//inherit from GameObject
+			oi=I.Init;//preserve reference to old init function
+			os=I.Sz;//preserve reference to old size function
+			I.Init=function(x,y,s,ti,o) {
+				o=oi(x,y,o);//call parent initializer
+				I.ti=ti;
+				I.S=s||[[0,0,[0]]];//animation spritesheet (atlas index, framerate, frames)
+				_ft=0;
+				return o;//invie overloading
+			};
+			I.Drw=function(dt, df){//draw: deltatime, draw function
+				var ss=I.S[I.ti];//spritesheet for current animation
+				df(I.x,I.y,ss[0],ss[2][I.f],
+					{sc:I.sc,al:I.al,ng:I.ng,ox:I.ox,oy:I.oy,fx:I.fx,fy:I.fy});
+			};
+			I.Ud=function(dt){//update: deltatime
+				//progress sprite animations
+				var fl,nf,
+				ss=I.S[I.ti];//current spritesheet
+				if(_ft>=0&&ss[1]){//as long as the animation isn't paused and has a valid length
+					_ft+=dt;//add deltatime to frame timer
+					fl=1/ss[1];//the expected total frame length (how many ms should pass before next frame)
+					if(_ft>fl){//if frame timer has advanced enough to move to the next frame...
+						nf=(I.f+1)%ss[2].length;//get index of the next frame for the current animation
+						if(ss[2][nf]<0) {//if the next frame is a pause frame
+							_ft=-1;//pause the animation on the current frame
+						}else{
+							I.f=nf;//otherwise loop the animation
+							_ft=_ft%fl;//get frame timer remainder (rather setting to 0) for more accurate timing
+						}
+					}
+				}
+			};
+			I.Pa=function(ti,f,r){//Play anim: set current anim/frame, r=force reset frame timer
+				ti=ti||0;
+				I.f=f||I.f||0;
+				if(ti!=I.ti||r){_ft=0;I.f=f||0;}//reset timer if animation has changed or the reset is forced
+				I.ti=ti;
+			};
+			I.Ta=function(){_ft=_ft<0?0:-1;};//Toggle Animation: switches between playing and stopped
+			I.Sz=function(){//get sprite size
+				return os(I.S[I.ti][0]);
+			};
+			I.Init(x,y,s,ti,o);//initialize
+			I.Pa(ti,o.f);//set up animation
+		};
 		//text: x, y, texture atlas index, text, options
-		//options: mw - max width (in characters, before line break), c - cipher string, al - alpha, sc - scale
-		N.Txt=function(x,y,a,t,o){
-			var I=this,cphr;
-			I.Init=function(x,y,a,t,o) {
-				o=o||{};
-				I.x=x;//x position
-				I.y=y;//y position
-				I.a=a;
-				I.t=t;
-				I.mw=o.mw||0;
-				I.al=o.al||1;
-				I.sc=o.sc||1;
+		//options: mw - max line width (in characters), c - cipher string, al - alpha, sc - scale
+		N.Txt=function(x,y,ti,t,o){
+			var I=this,oi,os,cphr;
+			_Go.call(I,x,y,o);//inherit from GameObject
+			oi=I.Init;//preserve reference to old init function
+			os=I.Sz;//preserve reference to old size function
+			I.Init=function(x,y,ti,t,o) {
+				o=oi(x,y,o);//call parent initializer
+				I.ti=ti;//texture atlas index (for character set)
+				I.t=t;//"text" string (can be character string or array of indexes)
+				I.mw=o.mw||0;//max line width (in characters)
 
 				if(o.c){
 					cphr={};//initialize cipher object
@@ -182,14 +259,24 @@ var Njs=function(id,cw,ch,aisrc,ta,fr,gs,aa){ //_ implies important hidden membe
 						cphr[o.c.charAt(i)]=i;//create mapping from character to index
 					}
 				}
+				return o;//invie overloading
+			};
+			I.Sz=function(){//get sprite size
+				var tsz=os(I.ti),//get size of tiles
+				w=tsz[0],h=tsz[1],//shorthand into width and height
+				ln=I.t.length;//text length shorthand
+				//return tile size multiplied by the width/height in tiles of the text element
+				return [I.mw?w*Math.min(ln,I.mw):w*ln,
+					I.mw?h*Math.ceil(ln/I.mw):h];
 			};
 			I.Drw=function(dt, df){//draw: deltatime, draw function
 				var i,x,y,
 				sc=N.GS*I.sc,//total scale
-				a=ta[I.a],//get texture atlas row
+				a=N.Ta[I.ti],//get texture atlas row
 				//shorthand
 				t=I.t,
-				w=I.mw;
+				w=I.mw,
+				odm=I.Sz();//get overall dimensions
 				if(!Array.isArray(t)){//assume that if t isn't an array, it must be a valid string
 					t=[];//convert the string to an array using the cipher
 					for(i=0;i<I.t.length;i++){
@@ -207,66 +294,9 @@ var Njs=function(id,cw,ch,aisrc,ta,fr,gs,aa){ //_ implies important hidden membe
 					//multiply indexes by offset amount
 					x*=a[3];//*sc;
 					y*=a[4];//*sc;
-					df(I.x+x,I.y+y,I.a,t[i],{al:I.al,sc:I.sc});//draw characters
+					df(I.x+x,I.y+y,I.ti,t[i],{al:I.al,sc:I.sc,ox:I.ox,oy:I.oy,ow:odm[0],oh:odm[1]});//draw characters
 				}
 			};
-			I.Init(x,y,a,t,o);//initialize
-			_Gol.push(I);
-		}
-		//sprite: x, y, spritesheet, current animation, options
-		//options: f=current frame, ox=origin offset x, oy=origin offset y, fx=x-flip, fy=y-flip
-		N.Spr=function(x,y,s,a,o){//a,f,co,sc,fx,fy
-			var I=this;
-			I.Init=function(x,y,s,a,o) {
-				o=o||{};
-				I.x=x;//x position
-				I.y=y;//y position
-				I.ox=o.ox||0;
-				I.oy=o.oy||0;
-				I.fx=!!o.fx;//flip x?
-				I.fy=!!o.fy;//flip y?
-				I.ng=o.ng||0;//rotation angle
-				I.sc=o.sc||1;//individual sprite scale
-				I.al=isNaN(o.al)?1:o.al;//sprite alpha
-				I.S=s||[[0,0,[0]]];//animation spritesheet (atlas index, framerate, frames)
-			};
-			I.Drw=function(dt, df){//draw: deltatime, draw function
-				var ss=I.S[I.a];//spritesheet for current animation
-				df(I.x,I.y,ss[0],ss[2][I.f],
-					{sc:I.sc,al:I.al,ng:I.ng,ox:I.ox,oy:I.oy,fx:I.fx,fy:I.fy});
-			};
-			I.Ud=function(dt){//update: deltatime
-				//progress sprite animations
-				var fl,nf,
-				ss=I.S[I.a];//current spritesheet
-				if(I._ft>=0&&ss[1]){//as long as the animation isn't paused and has a valid length
-					I._ft+=dt;//add deltatime to frame timer
-					fl=1/ss[1];//the expected total frame length (how many ms should pass before next frame)
-					if(I._ft>fl){//if frame timer has advanced enough to move to the next frame...
-						nf=(I.f+1)%ss[2].length;//get index of the next frame for the current animation
-						if(ss[2][nf]<0) {//if the next frame is a pause frame
-							I._ft=-1;//pause the animation on the current frame
-						}else{
-							I.f=nf;//otherwise loop the animation
-							I._ft=I._ft%fl;//get frame timer remainder (rather setting to 0) for more accurate timing
-						}
-					}
-				}
-			};
-			I.Pa=function(a,f,r){//Play anim: set current anim/frame, r=force reset frame timer
-				a=a||0;
-				I.f=f||I.f||0;
-				if(a!=I.a||r){I._ft=0;I.f=f||0;}//reset timer if animation has changed or the reset is forced
-				I.a=a;
-			};
-			I.Ta=function(){I._ft=I._ft<0?0:-1;};//Toggle Animation: switches between playing and stopped
-			I.Sz=function(){//get sprite size
-				if(!N._ai)return [0,0];//safety
-				var sa=ta[I.S[I.a][0]];//sprite atlas for the current animation
-				return [sa[3]*N.GS,sa[4]*N.GS];//return width,height of current sprite, scaled
-			};
-			I.Init(x,y,s,a,o);//initialize
-			I.Pa(a,o.f);//set up animation
-			_Gol.push(I);
-		}
+			I.Init(x,y,ti,t,o);//initialize
+		};
 	};
